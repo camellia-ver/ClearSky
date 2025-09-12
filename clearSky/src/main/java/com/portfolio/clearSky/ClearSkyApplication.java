438a687a2cfb38338db6ec21eb68557e5641f084
camellia-ver <jakahi435@gmail.com>
2025-09-12 22:26:39 +0900

    (추가) java spring 프로젝트 패키지 추가

---
+++ clearSky/src/main/java/com/portfolio/clearSky/ClearSkyApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.clearSky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClearSkyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClearSkyApplication.class, args);
+	}
+
+}
