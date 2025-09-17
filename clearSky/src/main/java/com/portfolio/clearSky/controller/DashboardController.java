80f59dc34acc99e84ae1324cf304a13173461e81
camellia-ver <jakahi435@gmail.com>
2025-09-17 11:16:03 +0900

    (추가) dashboard UI 추가

---
+++ clearSky/src/main/java/com/portfolio/clearSky/controller/DashboardController.java
@@ -0,0 +1,13 @@
+package com.portfolio.clearSky.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class DashboardController {
+    @GetMapping("/dashboard")
+    public String dashboard(@PathVariable String region){
+        return "dashboard";
+    }
+}
